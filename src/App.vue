08d38d8afe50fdf3fc1e70a383d2a0b001ea3d0c
splanaspe <splanaspe@uoc.edu>
2024-02-21 16:41:55 +0100

    Primer push

---
+++ src/App.vue
@@ -0,0 +1,223 @@
+<script setup>
+    import {ref, reactive, watch} from 'vue'
+    import Presupuesto from './components/Presupuesto.vue';
+    import ControlPresupuesto from './components/ControlPresupuesto.vue';
+    import Modal from './components/Modal.vue';
+    import Gasto from './components/Gasto.vue';
+
+    import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
+    import {generarId} from './helpers/index'
+
+    const presupuesto = ref(0);
+    const disponible = ref(0);
+    const gastado = ref(0)
+    let prueba;
+
+    const modal = reactive({
+        mostrar:false,
+        animar:false
+    });
+
+    const gasto = reactive({
+        nombre: '',
+        cantidad: '',
+        categoria: '',
+        id: null,
+        fecha: Date.now()
+    })
+
+    const gastos = ref([])
+
+    watch( gastos, () =>{
+        const totalGastado = gastos.value.reduce( (total,gasto) => gasto.cantidad + total, 0)
+        gastado.value=totalGastado
+        disponible.value = presupuesto.value - totalGastado
+    }, {
+        deep:true
+    })
+
+    const definirPresupuesto = (valor) => {
+        presupuesto.value=valor
+        disponible.value=valor
+    }
+
+    const mostrarModal = () =>{
+        modal.mostrar=true;
+        setTimeout( () => {
+            modal.animar=true;
+        },300)
+    }
+
+    const ocultarModal = () =>{
+        modal.animar=false;
+        setTimeout( () => {
+            modal.mostrar=false;
+        },300)
+    }
+
+    const guardarGasto = () =>{
+        gastos.value.push({
+            ...gasto,
+            id: generarId()
+        })
+
+        ocultarModal()
+
+        Object.assign(gasto, {
+            nombre: '',
+            cantidad: '',
+            categoria: '',
+            id: null,
+            fecha: Date.now()
+        })
+    }
+
+</script>
+
+<template>
+    <div
+        :class="{fijar: modal.mostrar}"
+    > 
+        <header> 
+            <h1 > Planificador de Gastos</h1>
+            
+            <div class="contenedor-header contenedor sombra">
+                <Presupuesto
+                    v-if="presupuesto === 0"
+                    @definir-presupuesto="definirPresupuesto"
+                ></Presupuesto>
+                <ControlPresupuesto
+                    v-else
+                    :presupuesto="presupuesto"
+                    :disponible="disponible"
+                    :gastado="gastado"
+                >
+                    
+                </ControlPresupuesto>
+            </div>
+        </header>
+        <main v-if="presupuesto > 0">
+
+            <div class="listado-gastos contenedor">
+                <h2>  {{gastos.length > 0 ? 'Gastos' : 'No hay gastos'}} </h2>
+                
+                <Gasto
+                    v-for="gasto in gastos"
+                    :key="gasto.id"
+                    :gasto="gasto"
+                />
+            </div>
+
+            <div class="crear-gasto">
+                <img :src="iconoNuevoGasto"
+                    alt="icono nuevo gasto"
+                    @click="mostrarModal"
+                >
+                
+            </div>
+            <Modal
+                v-if="modal.mostrar"
+                @ocultar-modal="ocultarModal"
+                @guardar-gasto="guardarGasto"
+                :modal="modal"
+                :disponible="disponible"
+                v-model:nombre="gasto.nombre"
+                v-model:cantidad="gasto.cantidad"
+                v-model:categoria="gasto.categoria"
+            />
+                
+        </main>
+    </div>
+</template>
+
+<style lang="scss"> 
+    :root {
+        --azul: #3b82f6;
+        --blanco: #FFF;
+        --gris-claro: #F5F5F5;
+        --gris: #94a3b8;
+        --gris-oscuro: #64748b;
+        --negro: #000;
+    }
+
+    html{
+        font-size:62.5%;
+        box-sizing: border-box;
+    }
+
+    *,
+    *:before,
+    *:after {
+        box-sizing: inherit;
+    }
+
+    body{
+        font-size: 1.6rem;
+        font-family: "Lato", sans-serif;
+        background-color: var(--gris-claro);
+    }
+    h1{
+        font-size: 4rem;
+    }
+
+    h2{
+        font-size: 3rem;
+    }
+
+    .fijar{
+        overflow: hidden;
+        height: 100vh;
+    }
+
+    header{
+        background-color: var(--azul);
+    }
+
+    header h1{
+        padding: 3rem 0;
+        margin: 0;
+        color: var(--blanco);
+        text-align: center;
+    }
+
+    .contenedor{
+        width: 90%;
+        max-width: 90rem;
+        margin: 0 auto;
+    }
+
+    .contenedor-header{
+        margin-top: -5rem;
+        transform: translateY(5rem);
+        padding: 5rem; 
+    }
+
+    .sombra{
+        // bow-shadow.dev para diseñar la sombra correcta
+        box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
+        background-color: white;
+        border-radius: 1.2rem;
+        padding: 5rem;
+    }
+
+    .crear-gasto {
+        position: fixed;
+        bottom: 5rem;
+        right: 5rem;
+    }
+
+    .crear-gasto img{
+        width: 5rem;
+        cursor: pointer;
+    }
+
+    .listado-gastos {
+        margin-top: 10rem;
+    }
+
+    .listado-gastos h2{
+        font-weight: 900;
+        color: var(--gris-oscuro)
+    }
+
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,205 @@
+<script setup>
+
+    import {ref} from  'vue'
+    import cerrarModal from './../assets/img/cerrar.svg'
+    import Alerta from './Alerta.vue';
+
+    const error=ref('')
+
+    const emit = defineEmits(['ocultar-modal', 'guardar-gasto','update:nombre', 'update:cantidad', 'update:categoria']);
+
+    const props = defineProps({
+        modal: {
+            type: Object,
+            required:true
+        },
+        nombre: {
+            type: String,
+            required: true
+        },
+        cantidad: {
+            type: [String, Number],
+            required: true
+        },
+        nombre: {
+            type: String,
+            required: true
+        },
+        categoria: {
+            type: String,
+            required: true
+        },
+        disponible:{
+            type: Number,
+            required: true
+        }
+    })
+
+    const agregarGasto = () => {
+        // Validar
+        const {cantidad, categoria, nombre, disponible} = props
+        if([nombre,cantidad,categoria].includes('')){
+            error.value= 'Todos los campos son obligatorios'
+            setTimeout( () => {
+                error.value=""
+            },2000)
+            return
+        }
+
+        if(cantidad <= 0){
+            error.value="Cantidad no válida"
+            setTimeout( () => {
+                error.value=""
+            },2000)
+            return
+        }
+        console.log("Tienes disponible: "+disponible)
+        //Validar que el usuario no gaste mas de lo disponible
+        if(cantidad > disponible){
+            error.value="Has excedido el presupuesto"
+            setTimeout( () => {
+                error.value=""
+            },2000)
+            return
+        }
+        emit('guardar-gasto')
+    }
+
+</script>
+
+<template>
+    <div class="modal">
+        <div class="cerrar-modal">
+            <img :src="cerrarModal"
+            @click="$emit('ocultar-modal')"> 
+        </div>
+        <div 
+            class="contenedor contenedor-formulario"
+            :class="[modal.animar ? 'animar' : 'cerrar']"
+        >
+            <form 
+                class="nuevo-gasto"
+                @submit.prevent="agregarGasto"
+            > 
+                <legend> Añadir Gasto </legend>
+                <Alerta v-if="error"> 
+                    {{ error }}
+                </Alerta>
+                <div class="campo">
+                    <label for="nombre"> Nombre Gasto </label>
+                    <input
+                        type="text"
+                        id="nombre"
+                        placeholder="Añade el nombre del gasto"
+                        :value="nombre"
+                        @input="$emit('update:nombre', $event.target.value)"
+                    >
+                </div>
+                <div class="campo">
+                    <label for="cantidad"> Cantidad </label>
+                    <input
+                        type="number"
+                        id="cantidad"
+                        placeholder="Añade la cantidad del gasto"
+                        :value="cantidad"
+                        @input="$emit('update:cantidad', +$event.target.value)"
+                    >
+                </div>
+                <div class="campo">
+                    <label for="categoria">Categoria: </label>
+                    <select 
+                        id="categoria"
+                        :value="categoria"
+                        @input="$emit('update:categoria', $event.target.value)"
+                    >
+                        <option vale="">-- Seleccione --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="ocio">Ocio</option>
+                        <option value="salud">Salud</option>
+                        <option value="suscripciones">Suscripciones</option>
+                    </select>
+                </div>
+                <input type="submit"
+                    value="Añadir Gasto"> 
+            </form>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .modal{
+        position:absolute;
+        background-color: rgb(0 0 0 /0.9);
+        top:0;
+        left:0;
+        right:0;
+        bottom: 0;
+    }
+
+    .cerrar-modal {
+        position: absolute;
+        right: 3rem;
+        top: 3rem;
+    }
+
+    .contenedor-formulario{
+        transition-property: all;
+        transition-duration: 300ms;
+        transition-timing-function: ease-in;
+    }
+
+    .contenedor-formulario.animar{
+        opacity: 1;
+    }
+
+    .contenedor-formulario.cerrar{
+        opacity: 0;
+    }
+
+    .cerrar-modal img {
+        width: 3rem;
+        cursor: pointer;
+    }   
+
+    .nuevo-gasto{
+        margin: 10rem auto 0 auto;
+        display: grid;
+        gap:2rem;
+    }
+
+    .nuevo-gasto legend {
+        text-align: center;
+        color: var(--blanco);
+        font-size: 3rem;
+        font-weight: 700;
+    }
+
+    .campo {
+        display:grid;
+        gap: 2rem;
+    }
+
+    .nuevo-gasto input,
+    .nuevo-gasto select {
+        background-color: var(--gris-claro);
+        border-radius: 1rem;
+        padding: 1rem;
+        border:none;
+        font-size: 2.2rem;
+    }
+
+    .nuevo-gasto label {
+        color: var(--blanco);
+        font-size:3rem;
+    }
+
+    .nuevo-gasto input[type="submit"] {
+        background-color: var(--azul);
+        color: var(--blanco);
+        font-weight: 700;
+        cursor: pointer;
+    }
+
+</style>
